c1b40d44493e5f566bfbeeec21fab573996a1b35
Ben Joy <ben.joy1@sky.com>
2018-04-14 10:30:19 +0100

    Add get current location

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,10 @@
 (function($, root, undefined) {
 	$(function() {
-		console.log("Hi");
+		 if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+				console.log("lat: " + position.coords.latitude);
+				console.log("lng: " + position.coords.longitude);
+            });
+        }
 	});
 })(jQuery, this);
